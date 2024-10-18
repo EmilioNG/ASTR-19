0c20051f0834fb91aacec48da95074e9f846388d
EmilioNG <enavar24@ucsc.edu>
2024-10-17 19:23:10 -0700

    Update session1_prompt.py

--- session1_prompt.py
+++ session1_prompt.py
@@ -1,9 +1,13 @@
-Date: 10/02/2024
+Date: "10/02/2024"
+
 #print full name
 print("Emilio Navarro Garza")
+
 #print pronouns
 print("He/Him")
+
 #print favorite movie
 print("My favorite movie is Interstellar")
+
 #print favorite food
 print("My favorite food are Doritos Incognita")
