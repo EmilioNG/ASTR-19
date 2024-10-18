a5cfc2f8f9d0a7b1740f9a6ea1ae2636937d9b28
EmilioNG <enavar24@ucsc.edu>
2024-10-17 23:50:56 -0700

    Add files via upload

---
+++ session3_prompt.py
@@ -0,0 +1,13 @@
+Date: "10/09/24"
+
+def f(x):
+    return((x ** 3) + 8)
+
+def main():
+    result = f(9)
+    print(result)
+    if result > 27:
+        print("YAY!")
+        
+if __name__=="__main__":
+    main()
