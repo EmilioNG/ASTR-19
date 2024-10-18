e16e503b7468ca029cc937322e06d0419706867d
EmilioNG <enavar24@ucsc.edu>
2024-10-17 23:38:26 -0700

    Add files via upload

---
+++ session2_prompt.py
@@ -0,0 +1,33 @@
+Date: "10/05/2024"
+
+#define 2 variables which values are floating point numbers
+f1 = 10.0
+f2 = 4.2
+#Add these variables
+f3 = f1 + f2
+
+#print the value of the sum of these numbers and the type
+print(f3)
+print(type(f3))
+
+#define another 2 variables that equal 2 integers
+i1 = 25
+i2 = 13
+#Substract these variables
+i3 = i1 - i2
+
+#print the value of the difference and the type
+print(i3)
+print(type(i3))
+
+#define 2 variables
+
+#define another 2 variables where one equals an integer and another a floating point number
+a = 6
+b = 8.1
+#multiply these variables
+c = a * b
+
+#print the value of the product and its type
+print(c)
+print(type(c))
