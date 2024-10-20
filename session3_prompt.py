e0eb1f88ff4576384338ddcc6766cf5c7a258e0f
EmilioNG <enavar24@ucsc.edu>
2024-10-19 19:58:55 -0700

    Update session3_prompt.py

--- session3_prompt.py
+++ session3_prompt.py
@@ -1,13 +1,15 @@
 Date: "10/09/24"
 
+#define an equation and the value it will return
 def f(x):
     return((x ** 3) + 8)
 
+#define the main function in which x will be defined
 def main():
     result = f(9)
     print(result)
     if result > 27:
         print("YAY!")
-        
+#initialize the code        
 if __name__=="__main__":
     main()
